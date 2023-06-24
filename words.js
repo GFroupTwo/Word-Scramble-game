844e061554bd110a55ed26d76acfc5448e08a2bd
Marilyn Uffort <uffortmarilyn@gmail.com>
2023-06-24 15:02:16 +0000

    added words.js

---
+++ words.js
@@ -0,0 +1,265 @@
+const words = [
+    {
+        word: "Butterfly",
+        hint: "- An insect with colorful wings"
+    },
+
+    {
+        word: "Chocolate",
+        hint: "- A sweet, delicious treat made from cocoa beans"
+    },
+
+    {
+        word: "Rainbow ",
+        hint: "- A colorful arch in the sky after rain"
+
+    },
+
+    {
+        word: "Ubiquitous",
+        hint: "- Present or found everywhere at the same time."
+    },
+
+    {
+        word: "Empathy ",
+        hint: "- Understanding and sharing the feelings of others."
+    },
+
+    {
+        word: "Ocean ",
+        hint: "- A vast body of saltwater"
+    },
+
+    {
+        word: "Pizza ",
+        hint: "- A savory dish with a thin crust, cheese, and various toppings."
+    },
+
+    {
+        word: "Coffee ",
+        hint: "- A popular caffeinated beverage"
+    },
+
+    {
+        word: "Eloquent ",
+        hint: "- Fluent, persuasive, and expressive in speech or writing."
+    },
+
+    {
+        word: "Carrot",
+        hint: "- An orange vegetable with a crunchy texture."
+    },
+
+    {
+        word: "Soccer",
+        hint: "- A popular sport played with a ball"
+    },
+
+    {
+        word: "Ambiguous",
+        hint: "- Having more than one possible interpretation or meaning."
+    },
+
+    {
+        word: "Serene",
+        hint: "- Calm, peaceful, and untroubled."
+    },
+
+    {
+        word: "Deceptive",
+        hint: "- Giving an appearance or impression that is misleading."
+    },
+
+    {
+        word: "Crispy",
+        hint: "- Having a crunchy and brittle texture when cooked or fried."
+    },
+
+    {
+        word: "Nutritious",
+        hint: " - Providing valuable nutrients and nourishment for the body."
+    },
+
+    {
+        word: "Procrastinate",
+        hint: "- To delay or postpone action, often out of laziness or indecision."
+    },
+
+    {
+        word: "Drizzle",
+        hint: "- Light rain falling steadily in fine, mist-like droplets."
+    },
+
+    {
+        word: "Exquisite",
+        hint: "- Extremely beautiful, delicate, or finely crafted."
+    },
+
+    {
+        word: "Ponder",
+        hint: "- To think deeply and carefully about something."
+    },
+
+    {
+        word: "Exemplary",
+        hint: "- Serving as a desirable model; commendable."
+    },
+
+    {
+        word: "Gracious",
+        hint: "- Courteous, kind, and pleasant, especially towards others."
+    },
+
+    {
+        word: "Zeal",
+        hint: "- Enthusiastic and dedicated pursuit of a cause or objective."
+    },
+
+    {
+        word: "Elephant",
+        hint: "- A large, gray mammal with a long trunk."
+    },
+
+    {
+        word: "Bicycle",
+        hint: " - A two-wheeled vehicle you pedal."
+    },
+
+    {
+        word: "Guitar",
+        hint: "- A stringed musical instrument."
+    },
+
+    {
+        word: "Keyboard",
+        hint: "- A device used to type on a computer."
+    },
+
+    {
+        word: "Dichotomy",
+        hint: "- A division or contrast between two opposing things or ideas."
+    },
+
+    {
+        word: "Apathy",
+        hint: "- Lack of interest, enthusiasm, or concern."
+    },
+
+    {
+        word: "Water",
+        hint: "A colorless, transparent, odorless liquid that is essential for life."
+    },
+
+    {
+        word: "Succulent",
+        hint: "- Juicy and tender, especially in reference to meat or fruits."
+    },
+
+    {
+        word: "Tangy",
+        hint: "- Having a sharp, acidic, or pleasantly tart taste."
+    },
+
+    {
+        word: "Frugal",
+        hint: "- Economical in the use of resources; thrifty."
+    },
+
+    {
+        word: "Alleviate",
+        hint: "- To make (suffering, pain, or hardship) less severe."
+    },
+
+    {
+        word: "Reconcile",
+        hint: "- To restore friendly relations between; bring into agreement or harmony."
+    },
+
+    {
+        word: "Sagacious",
+        hint: "- Wise, shrewd, and discerning."
+    },
+
+    {
+        word: "Savory",
+        hint: "- Having a rich, umami flavor; opposite of sweet."
+    },
+
+    {
+        word: "Adventure",
+        hint: "- An exciting or daring experience; a journey filled with unknown outcomes."
+    },
+
+    {
+        word: "Illuminate",
+        hint: "- To light up or brighten; to clarify or make something understandable."
+    },
+
+    {
+        word: "Curiosity",
+        hint: "- A strong desire to learn, explore, or know about something new or unknown."
+    },
+
+    {
+        word: "Enthusiasm",
+        hint: "- Intense excitement, interest, or eagerness about something."
+    },
+
+    {
+        word: "Epiphany",
+        hint: "- A sudden realization or understanding of something significant."
+    },
+
+    {
+        word: "Solitude",
+        hint: "- The state of being alone or secluded, often for contemplation or relaxation."
+    },
+
+    {
+        word: "Enigma",
+        hint: "- A puzzle or mystery; something difficult to understand or explain."
+    },
+
+    {
+        word: "Scrumptious",
+        hint: "- Extremely tasty and appetizing."
+    },
+
+    {
+        word: "Abundance",
+        hint: "- A large quantity or supply of something."
+    },
+
+    {
+        word: "Horse",
+        hint: "- A large, hoofed mammal often used for riding or racing."
+    },
+
+    {
+        word: "Flower",
+        hint: "- A colorful plant that blooms and adds beauty to nature."
+    },
+
+    {
+        word: "Lament",
+        hint: "- To express grief or sorrow; mourn."
+    },
+
+    {
+        word: "Star",
+        hint: "- A luminous celestial body visible in the night sky."
+    },
+
+    {
+        word: "Cake",
+        hint: "- A sweet baked dessert."
+    },
+
+   
+
+    
+
+    
+
+
+]
